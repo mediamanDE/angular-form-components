dfc10008db016aeeccf55cfe6fcc29d560ad94fc
Marcel Kempf <marcel.kempf93@gmail.com>
2018-09-24 15:16:02 +0200

    Add checkbox to list of exported components

--- src/angular-form-components.ts
+++ src/angular-form-components.ts
@@ -5,5 +5,6 @@ export { SelectComponent } from './select/select.component';
 export { RadioButtonComponent } from './radio-button/radio-button.component';
 export { RadioButtonGroupComponent } from './radio-button/radio-button-group.component';
 export { ToggleComponent } from './toggle/toggle.component';
+export { CheckBoxComponent } from './checkbox/checkbox.component';
 export { AngularFormComponentsModule } from './angular-form-components.module';
 export { AbstractFormControl } from './abtract-form-control';
